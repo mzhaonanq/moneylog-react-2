b032e0ed805873cd01a452c4466014dab085ca81
mzhaonanq <1002604003@qq.com>
2020-12-07 17:34:19 +0800

    路由配置完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,63 @@
 import React from 'react';
+import {BrowserRouter as Router, Link, Redirect, Route, Switch, useLocation} from 'react-router-dom';
 
-
-function App() {
+export default function App() {
   return (
-    <div>你好</div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/tags">标签</Link>
+            </li>
+            <li>
+              <Link to="/money">记账</Link>
+            </li>
+            <li>
+              <Link to="/statistics">统计</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/money"/>
+          </Route>
+          <Route path="/tags">
+            <Tags/>
+          </Route>
+          <Route path="/money">
+            <Money/>
+          </Route>
+          <Route path="/statistics">
+            <Statistics/>
+          </Route>
+          <Route path="*">
+            <NoMatch/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Statistics() {
+  return <h2>统计页</h2>;
+}
+
+function Tags() {
+  return <h2>标签页</h2>;
+}
+
+function Money() {
+  return <h2>记账页</h2>;
+}
+function  NoMatch(){
+  let location = useLocation();
+  return (
+    <div>
+      <h3>
+        No match for <code>{location.pathname}</code>
+      </h3>
+    </div>
+  );
+}
