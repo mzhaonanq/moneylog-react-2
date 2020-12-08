6061addd6ad2f7286caf2d008c7e4a5054fb50af
mzhaonanq <1002604003@qq.com>
2020-12-08 15:26:58 +0800

    扩大导航栏点击范围

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -7,24 +7,30 @@ import Icon from './Icon';
 const Wrapper = styled.nav`
   border: 1px solid green;
   line-height: 24px;
-  box-shadow: 0 0 3px rgba(0,0,0,0.25);
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+
   > ul {
     display: flex;
+
     > li {
       width: 33.3333%;
       text-align: center;
       padding: 4px 0;
+
+      > a {
       display: flex;
       flex-direction: column;
       align-items: center;
-
-      > .icons {
-        width: 24px;
-        height: 24px;
+        > .icons {
+          width: 24px;
+          height: 24px;
+        }
       }
-
-      > .selected {
-        color: coral;
+      & .selected { 
+        color: chartreuse;
+        > .icons{
+          fill:red;
+        }
       }
     }
   }
@@ -35,16 +41,22 @@ const Nav =()=>{
     <Wrapper>
       <ul>
         <li>
-          <Icon name="tags"/>
-          <NavLink to="/tags" activeClassName="selected">标签</NavLink>
+          <NavLink to="/tags" activeClassName="selected">
+            <Icon name="tags"/>
+            标签
+          </NavLink>
         </li>
         <li>
-          <Icon name="money"/>
-          <NavLink to="/money" activeClassName="selected">记账</NavLink>
+          <NavLink to="/money" activeClassName="selected">
+            <Icon name="money"/>
+            记账
+          </NavLink>
         </li>
         <li>
-          <Icon name="statistics"/>
-          <NavLink to="/statistics" activeClassName="selected">统计</NavLink>
+          <NavLink to="/statistics" activeClassName="selected">
+            <Icon name="statistics"/>
+            统计
+          </NavLink>
         </li>
       </ul>
     </Wrapper>
