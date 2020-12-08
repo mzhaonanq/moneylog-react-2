03c9bb30e76edc41a4d126ccb8a18e183f98b76c
mzhaonanq <1002604003@qq.com>
2020-12-08 16:17:45 +0800

    Money页骨架完成

--- src/Views/Money.tsx
+++ src/Views/Money.tsx
@@ -4,8 +4,42 @@ import React from 'react';
 const Money =()=> {
   return (
     <Layout>
-      <h2>记账页</h2>
+      <section>
+        <ul>
+          <li>衣</li>
+          <li>食</li>
+          <li>住</li>
+          <li>行</li>
+        </ul>
+        <button>新增标签</button>
+      </section>
+      <section>
+        <label>
+          <span>备注</span>
+          <input type="text"/>
+        </label>
+      </section>
+      <section>
+        <div>100</div>
+        <div>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>删除</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>清空</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>OK</button>
+          <button>0</button>
+          <button>.</button>
+        </div>
+      </section>
     </Layout>
   );
 }
+
 export default Money
