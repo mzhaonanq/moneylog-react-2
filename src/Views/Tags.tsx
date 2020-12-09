1b2e48a8b650b77514fe9f439ae7bdeca23a2452
mzhaonanq <1002604003@qq.com>
2020-12-09 21:46:51 +0800

    完成Tags.tsx组件样式

--- src/Views/Tags.tsx
+++ src/Views/Tags.tsx
@@ -1,14 +1,59 @@
 import {Layout} from '../Components/Layout';
 import React from 'react';
 import {useTags} from '../useTags';
+import styled from 'styled-components';
+import {Icon} from '../Components/Icon';
 
-const Tags: React.FunctionComponent=()=> {
-  const {tags,setTags}=useTags()
+const TagList = styled.ul`
+  background: #ffffff;
+
+  > li {
+    line-height: 20px;
+    padding: 12px 0;
+    margin: 0 16px;
+    border-bottom: 1px solid #d5d5d5;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
+
+const Button = styled.button`
+font-size: 18px;
+  border:none;
+  background: #767676;
+  color: #ffffff;
+  padding: 8px 12px;
+`
+
+const CenterWrapper = styled.div`
+display:flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const Space = styled.div`
+  height: 32px;
+`
+
+const Tags: React.FunctionComponent = () => {
+  const {tags, setTags} = useTags();
   return (
     <Layout>
-      <ul>
-        {tags.map(tag=><li key={tag}>{tag}</li>)}
-      </ul>
+      <TagList>
+        {tags.map(tag =>
+          <li key={tag}>
+            <span className='oneLine'>{tag}</span>
+            <Icon name='right'/>
+          </li>)}
+      </TagList>
+      <CenterWrapper>
+        <Space/>
+        <Space/>
+        <Space/>
+        <Button>新增标签</Button>
+      </CenterWrapper>
     </Layout>
   );
 }
