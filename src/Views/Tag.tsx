ee61e3ac1b48f51350719334dfc4224504a362fc
mzhaonanq <1002604003@qq.com>
2020-12-10 15:26:59 +0800

    完成Tag.tsx的骨架

--- src/Views/Tag.tsx
+++ src/Views/Tag.tsx
@@ -1,18 +1,40 @@
-import {useParams} from 'react-router-dom'
+import {useParams} from 'react-router-dom';
 import React from 'react';
 import {useTags} from '../useTags';
+import {Icon} from '../Components/Icon';
+import {Layout} from '../Components/Layout';
+import styled from 'styled-components';
 
 type Params = {
   tagId: string
 }
 
-const Tag: React.FunctionComponent=()=>{
-  const { tagId } = useParams<Params>()
-  const {findTagById} =useTags()
-  const tag = findTagById(parseFloat(tagId))[0]
-  return(
-    <div>{tag.name}</div>
-  )
+const Button = styled.button`
+font-size: 18px;
+  border:none;
+  background: #767676;
+  color: #ffffff;
+  padding: 8px 12px;
+`
+
+
+const Tag: React.FunctionComponent = () => {
+  const {tagId} = useParams<Params>();
+  const {findTagById} = useTags();
+  const tag = findTagById(parseFloat(tagId))[0];
+  return (
+    <Layout>
+      <header>
+        <Icon name='left'/>
+        <span>编辑标签</span>
+      </header>
+      <main>
+        <span>标签名</span>
+        <input type="text" placeholder="在这里输入标签名"/>
+      </main>
+      <Button>删除标签</Button>
+    </Layout>
+  );
 }
 
 export {Tag}
