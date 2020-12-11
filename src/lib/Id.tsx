3e2c2761836af44997f245fe7e63095417a25d41
mzhaonanq <1002604003@qq.com>
2020-12-11 11:44:56 +0800

    解决了刷新页面tag.id重置为0的bug

--- src/lib/Id.tsx
+++ src/lib/Id.tsx
@@ -1,9 +1,11 @@
-let id = 0
+let id =JSON.parse(window.localStorage.getItem('idMax')||'0' )
+console.log('执行了Id类');
 class Id {
   idValue: number;
   constructor() {
     id += 1;
     this.idValue = id
+    window.localStorage.setItem('idMax',JSON.stringify(this.idValue))
   }
 }
 
